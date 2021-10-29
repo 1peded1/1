3effbda73186c965554205d90ca143850499fdae
1peded1 <krekota.st@gmail.com>
2021-10-29 07:17:09 +0300

    first

--- С++.cpp
+++ С++.cpp
@@ -1,1 +1,24 @@
-include<iostream>
+#include <iostream> 
+#include <iomanip> 
+#include "windows.h"
+#include "math.h"
+
+using namespace std;
+
+int main()
+{
+	int x;
+	cout << "Enter x=";
+	cin >> x;
+	float rez;
+	int sum = 0;
+	for (int i = 1; i < 6; i++)
+	{
+		sum = sum + i + 2 * x;
+	}
+	rez = tan(sum);
+	cout << "Y=" << sum << endl;
+
+	system("pause");
+	return 0;
+}
